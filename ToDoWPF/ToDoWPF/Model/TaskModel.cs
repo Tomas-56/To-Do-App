c994701e81ac33b0b17390fc6d348b3d4129daeb
Tomas <tomasurban243@gmail.com>
2025-02-02 17:23:00 +0100

    pridanie Resources (Font,Farby, Template pre tlačítka),Task Modelu, Task Panel View

---
+++ ToDoWPF/ToDoWPF/Model/TaskModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoWPF.Model
+{
+    internal class TaskModel
+    {
+		private string _taskName;
+
+		public string TaskName
+		{
+			get { return _taskName; }
+			set { _taskName = value; }
+		}
+
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+		private bool _isDone;
+
+		public bool IsDone
+		{
+			get { return _isDone; }
+			set { _isDone = value; }
+		}
+
+
+
+	}
+}
